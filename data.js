456676550fe0536eef54d976522a4e95cb527ebf
developerNd <91623104+developerNd@users.noreply.github.com>
2022-08-16 17:57:26 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,35 @@
-Let menuItemsData = {
-          Cat1:[
-          {id:"this is id",
-          name: "product",
-          desc: "this is description",
-          price: 100}
-          ]
-        };
+// list of menu data 
+let menuItemsData = {
+  "cat1": [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "ioytrhndcvewrw",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    }
+  ],
+  "cat2": [
+    {
+      id: "ioytrhndcvwerwr",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+      {
+        id: "ioytrhndcvsdfs",
+        name: "Office Shirt",
+        price: 100,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-2.jpg",
+      }
+  ]
+};
